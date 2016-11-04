1f2d4a5cfd9fb1ca5182d8455c66db0041c8a89b
Forrest Zhao <fozhao92@gmail.com>
2016-11-04 14:36:06 -0400

    singleton class to store search requests

---
+++ FaveSpots/FaveSpots/SearchForPlaces.swift
@@ -0,0 +1,9 @@
+//
+//  SearchForPlaces.swift
+//  FaveSpots
+//
+//  Created by Forrest Zhao on 11/4/16.
+//  Copyright © 2016 ForrestApps. All rights reserved.
+//
+
+import Foundation
---
+++ FaveSpots/FaveSpots/SearchRequestDataSource.swift
@@ -0,0 +1,16 @@
+//
+//  SearchRequestDataSource.swift
+//  FaveSpots
+//
+//  Created by Forrest Zhao on 11/4/16.
+//  Copyright © 2016 ForrestApps. All rights reserved.
+//
+import MapKit
+
+class SearchRequestDataSource {
+    static let sharedInstance = SearchRequestDataSource()
+    private init() {}
+    
+    var searchRequests: [MKLocalSearchRequest] = []
+    
+}
